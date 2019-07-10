05259dbc80c080b56635464d1a99bc9736797386
kalinstoev <kalinstoev@fan.gov>
2019-07-10 18:33:44 +0300

    Added Login and Register forms

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -31,6 +31,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
