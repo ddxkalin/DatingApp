576d5d927000bbbcf86ab9fcef907a1ffca5cdb9
kalinstoev <kalinstoev@fan.gov>
2019-07-12 18:19:18 +0300

    Added Edit Profile Action

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -24,8 +24,9 @@ namespace DatingApp.API.Helpers
                 .ForMember(dest => dest.Age, opt => {
                     opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
                 });
-
+            
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
